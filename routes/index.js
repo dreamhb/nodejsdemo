46e5489464118d6131a432b4b93aa4e6e0301afb
Hu Bing <hubing@Hus-Mac-mini.local>
2015-11-10 22:48:56 +0800

    get GET params

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  var url_parts = url.parse(req.url, true);
+  var query = url_parts.query;
+  console.log("query name " + query.name + " color = " + query.color);
 });
 
 module.exports = router;
