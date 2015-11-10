b0b21d08ceea378277d53a5a463f99d404d9a62f
Hu Bing <hubing@Hus-Mac-mini.local>
2015-11-10 22:38:42 +0800

    post body

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var url = require("url");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
+});
+
+//post test
+router.post('/', function(req, res, next){
+    console.log(" Received POST data name = " + req.body.name
+        + " color = " + req.body.color);
+    res.send("END");
 });
 
 module.exports = router;
